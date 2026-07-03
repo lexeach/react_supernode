cc3d447c2a6e3f0e69e1542ec187b84c8cd57fb9
lexeach <80155340+lexeach@users.noreply.github.com>
2026-07-03 13:09:49 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,8 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 // 2. Local Project CSS (Ensure these files exist in src/assets/css/)
-import "./assets/css/bootstrap.min.css";
-import "./assets/css/app.css";
+//import "./assets/css/bootstrap.min.css";
+//import "./assets/css/app.css";
 import "./assets/css/responsive.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
